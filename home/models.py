b88b798709c687b859c4b63f9c802eb06d2ca93c
apoorvadesai2512-cpu <apoorvadesai2512@gmail.com>
2025-09-24 20:00:06 +0530

    50726

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class MenuCategory(models.Model):
+    name = models.charField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+        
