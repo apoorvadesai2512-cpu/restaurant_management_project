8c5488e034d3ab235f04dfc94dd3343661b66f8f
apoorvadesai2512-cpu <apoorvadesai2512@gmail.com>
2025-09-26 21:39:35 +0530

    50728

--- orders/models.py
+++ orders/models.py
@@ -3,11 +3,8 @@ from django.db import models
 # Create your models here.
 from home.models import OrderStatus
 
-class Order(models.Model):
-    customer_name = models.CharField(max_length=100)
-    order_date = models.DateTimeField(auto_now_add=True)
+class OrderStatus(models.Model):
+    name = models.CharField(max_length=50, unique=True)
 
-    status = models.ForeignKey(OrderStatus, on_delete=models.SET_NULL, null=True)
-
-    def __sef__(self):
-        return f"Order {self.id} - {self.customer_name}"
+    def __str__(self):
+        return self.name
