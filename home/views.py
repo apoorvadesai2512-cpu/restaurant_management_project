4000850e7675df2d538d5baf924bc3e5514f7943
apoorvadesai2512-cpu <apoorvadesai2512@gmail.com>
2025-09-28 21:48:13 +0530

    51813

--- home/views.py
+++ home/views.py
@@ -3,26 +3,26 @@ from django.shortcuts import render
 # Create your views here.
 
 from rest_framework import viewsets, filters
+from rest_framework.response import response
+from rest_framework import status
 from home.models import MenuItem
 from home.serializers import MenuItemSerializer
 from rest_framework.pagination import PageNumberPagination
 
-class MenuItemPagination(PageNumberPagination):
-    page_size = 10
-    page_size_query_param = 'page_size'
-    max_page_size = 100
+class MenuItemViewSet(viewsets.ModelViewSet):
 
-class MenuItemSearchViewSet(viewsets.ReadOnlyModelViewSet):
-
+    queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
-    pagination_class = MenuItemPagination
+    permission_classes = [permissions.IsAdminUser]
 
-    def get_queryset(self):
-        queryset = MenuItem.objects.all()
-        query = self.request.query_params.get('q')
-        if query:
-            queryset = queryset.filter(name__icontains=query)
-        return queryset
-        
+    def update(self, request, *args, **kwargs):
 
-    
+        try:
+            partial = kwargs.pop('partial', False)
+            instance = self.get_object()
+            serializer = self.get_serializer(instance, data=request.data, partial=partial)
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
+            return Response(serializer.data)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
