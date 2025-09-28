c5381cab31df27f568267b5d05d2f121ec6dc1a7
apoorvadesai2512-cpu <apoorvadesai2512@gmail.com>
2025-09-28 20:41:47 +0530

    51812

--- home/views.py
+++ home/views.py
@@ -2,12 +2,27 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from rest_framework.generics import ListAPIView
-from home.models import MenuCategory
-from home.serializers import MenuCategorySerializer
+from rest_framework import viewsets, filters
+from home.models import MenuItem
+from home.serializers import MenuItemSerializer
+from rest_framework.pagination import PageNumberPagination
 
-class MenuCategoryListView(ListAPIView):
-    queryset = MenuCategory.objects.all()
-    serializer_class = MenuCategorySerializer
+class MenuItemPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+class MenuItemSearchViewSet(viewsets.ReadOnlyModelViewSet):
+
+    serializer_class = MenuItemSerializer
+    pagination_class = MenuItemPagination
+
+    def get_queryset(self):
+        queryset = MenuItem.objects.all()
+        query = self.request.query_params.get('q')
+        if query:
+            queryset = queryset.filter(name__icontains=query)
+        return queryset
+        
 
     
