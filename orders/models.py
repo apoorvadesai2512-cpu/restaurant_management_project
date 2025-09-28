c4d9df8d571bee26197441cc3b1782471bc49eb7
apoorvadesai2512-cpu <apoorvadesai2512@gmail.com>
2025-09-28 20:11:00 +0530

    51816

--- orders/models.py
+++ orders/models.py
@@ -1,12 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.mosels import product 
 
 # Create your models here.
-class Coupon(models.Model):
-
-    code = models.CharField(max_length=20, unique=True)
-    discount = models.DecimalField(max_digits=5, decimal_places=2, default=0.000)
+class order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
     created_at = models.DateTimeField(auto_now_add=True)
+    total_pprice = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __dtr__(self):
+        return f"order {self.id} by {self.user.username}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return self.code
-        
+        return f"{self.quantity} x {self.product.name}"
