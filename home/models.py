8327c7be8c0fb1d677541d3412869d7c49aafe29
apoorvadesai2512-cpu <apoorvadesai2512@gmail.com>
2025-09-28 21:00:13 +0530

    51813

--- home/models.py
+++ home/models.py
@@ -6,4 +6,14 @@ class MenuCategory(models.Model):
 
     def __str__(self):
         return self.name
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    category = models.Foreignkey(MenuCategory, on_delete=models.CASCADE, related_name="items")
+
+    def __str__(self);
+        return self.name
         
